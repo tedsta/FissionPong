e8b93da963df05d57d3007ba342574ce3c546cac
Richard Kerr <rmkerr@hawaii.edu>
2014-01-10 18:03:45 -1000

    Added dimensions component

---
+++ include/Dimesions.h
@@ -0,0 +1,17 @@
+#ifndef DIMESIONS_H
+#define DIMESIONS_H
+
+
+class Dimesions
+{
+    public:
+        Dimesions(int x, int y);
+        ~Dimesions();
+
+        int mX;//Width
+        int mY;//Height
+    protected:
+    private:
+};
+
+#endif // DIMESIONS_H
---
+++ src/Dimesions.cpp
@@ -0,0 +1,11 @@
+#include "Dimesions.h"
+
+Dimesions::Dimesions(int x, int y) : mX(x),mY(y)
+{
+    //ctor
+}
+
+Dimesions::~Dimesions()
+{
+    //dtor
+}
